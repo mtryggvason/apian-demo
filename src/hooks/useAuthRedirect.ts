41556e52a9e1587dc9a9e3e4b5f546e1da87f168
mtryggvason <90194562+golottie-magnus@users.noreply.github.com>
2023-05-03 21:36:15 +0200

    fix build

--- src/hooks/useAuthRedirect.ts
+++ src/hooks/useAuthRedirect.ts
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
-export const useAuthRedirect = (auth) => {
+export const useAuthRedirect = (auth:any) => {
   const router = useRouter();
   useEffect(() => {
       auth.onAuthStateChanged(() => {
