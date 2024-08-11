b7f0d05401f531bdfd5239837205f948a0ffbd38
Magnus Tryggvason <magnus@apian.aero>
2024-08-11 23:19:39 +0200

    fix ios top bar

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,11 @@ export default function Document() {
   return (
     <Html lang="en">
       <title>Drone Spotter</title>
+      <meta
+        name="apple-mobile-web-app-status-bar-style"
+        content="black-translucent"
+      />
+      <meta name="apple-mobile-web-app-capable" content="yes"></meta>
       <link rel="manifest" href="/manifest.json" />
 
       <link
